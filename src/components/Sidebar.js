afd56d89f7c5cbaf2613a640aeee3bd26f6b2159
Putnam14 <putnambr76@gmail.com>
2019-03-10 21:41:33 -0600

    Setup SidebarStyles

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,17 @@
 import React from "react";
+import styled from "styled-components";
 
-const Sidebar = () => <p>Sidebar</p>;
+const SidebarStyles = styled.aside`
+  height: 100%;
+  width: 80px;
+  background-color: rgba(32, 32, 35, 1);
+  border-right: 1px solid rgba(189, 189, 192, 0.1);
+  transition: width 0.3s;
+  @media (min-width: 576px) {
+    width: 320px;
+  }
+`;
+
+const Sidebar = () => <SidebarStyles>Sidebar</SidebarStyles>;
 
 export default Sidebar;
