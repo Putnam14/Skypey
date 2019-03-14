94a1daaa7b346f0e4ec983d80bef6a047ca53901
Putnam14 <putnambr76@gmail.com>
2019-03-13 22:37:10 -0600

    Stub out the Empty component

---
+++ src/components/Empty.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const EmptyStyles = styled.div``;
+
+const Empty = () => {
+  return <EmptyStyles>Empty</EmptyStyles>;
+};
+
+export default Empty;
