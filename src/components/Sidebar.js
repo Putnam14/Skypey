7e0694752784435bf8e603cbfd9a6baaa7678f29
Putnam14 <putnambr76@gmail.com>
2019-03-13 22:12:38 -0600

    Create a contact list

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import User from "../containers/User";
 
 const SidebarStyles = styled.aside`
   height: 100%;
@@ -7,11 +8,21 @@ const SidebarStyles = styled.aside`
   background-color: rgba(32, 32, 35, 1);
   border-right: 1px solid rgba(189, 189, 192, 0.1);
   transition: width 0.3s;
+  overflow-y: scroll;
   @media (min-width: 576px) {
     width: 320px;
   }
 `;
 
-const Sidebar = () => <SidebarStyles>Sidebar</SidebarStyles>;
+const Sidebar = ({ contacts }) => {
+  console.log(contacts[0][1].name);
+  return (
+    <SidebarStyles>
+      {contacts.map(contact => (
+        <User user={contact[1]} key={contact[0]} />
+      ))}
+    </SidebarStyles>
+  );
+};
 
 export default Sidebar;
