86e9adb126ad9823e314adcb20c6ce54ef80a92f
Putnam14 <putnambr76@gmail.com>
2019-03-13 22:41:31 -0600

    Render the Empty component if activeUserId is null

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,14 @@
 import React from "react";
 import styled from "styled-components";
+import Empty from "../components/Empty";
 
 const MainStyles = styled.main`
   height: 100%;
   background-color: rgba(25, 25, 27, 1);
 `;
 
-const Main = () => <MainStyles>Main</MainStyles>;
+const Main = ({ user, activeUserId }) => (
+  <MainStyles>{!activeUserId && <Empty user={user} />}</MainStyles>
+);
 
 export default Main;
