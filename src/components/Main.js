e9fe4f7e30084d32d6919d5b98632b5dc570791c
Putnam14 <putnambr76@gmail.com>
2019-03-14 17:46:27 -0600

    Show Empty if no active user, chat window if there is

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Empty from "../components/Empty";
+import ChatWindow from "../containers/ChatWindow";
 
 const MainStyles = styled.main`
   height: 100%;
@@ -8,7 +9,13 @@ const MainStyles = styled.main`
 `;
 
 const Main = ({ user, activeUserId }) => (
-  <MainStyles>{!activeUserId && <Empty user={user} />}</MainStyles>
+  <MainStyles>
+    {activeUserId ? (
+      <ChatWindow activeUserId={activeUserId} />
+    ) : (
+      <Empty user={user} />
+    )}
+  </MainStyles>
 );
 
 export default Main;
