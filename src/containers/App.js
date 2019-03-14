c0872e903e6f40262b6dac0813cffe040addf123
Putnam14 <putnambr76@gmail.com>
2019-03-13 22:11:39 -0600

    Move App.js to the containers folder

---
+++ src/containers/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+import Main from "../components/Main";
+import Sidebar from "../components/Sidebar";
+import store from "../store";
+
+const AppStyles = styled.div`
+  height: 100%;
+  display: grid;
+  grid-template-columns: auto 1fr;
+  color: rgba(189, 189, 192, 1);
+`;
+
+const App = () => {
+  const { contacts } = store.getState();
+
+  return (
+    <AppStyles>
+      <Sidebar contacts={Object.entries(contacts)} />
+      <Main />
+    </AppStyles>
+  );
+};
+
+export default App;
