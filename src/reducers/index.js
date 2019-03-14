50e5e0ea4a7a906aa032ede738d93b89905192ce
Putnam14 <putnambr76@gmail.com>
2019-03-13 22:20:34 -0600

    Explain what the reducer combiner is doing

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,9 @@ import user from "./user";
 import contacts from "./contacts";
 import { combineReducers } from "redux";
 
+// This is representative of the state
+// Data comes from the reducers
+// e.g. the user property will get state data from the user reducer
 const combinedReducers = combineReducers({
   user,
   contacts
