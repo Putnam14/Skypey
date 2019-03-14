62c95c478fa79ae2b5118169a14f939db0e2507c
Putnam14 <putnambr76@gmail.com>
2019-03-14 17:53:27 -0600

    Style the Empty/user page

--- src/components/Empty.js
+++ src/components/Empty.js
@@ -2,9 +2,37 @@ import React from "react";
 import styled from "styled-components";
 
 const EmptyStyles = styled.div`
-  .status {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  flex-direction: column;
+  h1 {
+    color: #fff;
   }
+  img {
+    border-radius: 50%;
+    margin: 2rem 0;
+  }
+  button {
+    padding: 1rem;
+    margin: 1rem 0;
+    font-weight: bold;
+    font-size: 1.2rem;
+    border-radius: 30px;
+    outline: 0;
+    :hover {
+      background: rgba(255, 255, 255, 0.7);
+      cursor: pointer;
+    }
+  }
+  .status,
   .info {
+    padding: 1rem;
+  }
+  .status {
+    color: rgba(255, 255, 255, 0.9);
+    border-bottom: 1px solid rgba(255, 255, 255, 0.7);
   }
 `;
 
