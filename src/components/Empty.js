d66a7b74dbe5fa1fe3427ec0bfca10fe4e933f62
Putnam14 <putnambr76@gmail.com>
2019-03-14 17:43:30 -0600

    Populate the Empty component with User info

--- src/components/Empty.js
+++ src/components/Empty.js
@@ -1,10 +1,30 @@
 import React from "react";
 import styled from "styled-components";
 
-const EmptyStyles = styled.div``;
+const EmptyStyles = styled.div`
+  .status {
+  }
+  .info {
+  }
+`;
 
-const Empty = () => {
-  return <EmptyStyles>Empty</EmptyStyles>;
+const Empty = ({ user }) => {
+  const { name, profile_pic, status } = user;
+  const first_name = name.split(" ")[0];
+  return (
+    <EmptyStyles>
+      <h1>Welcome, {first_name}</h1>
+      <img src={profile_pic} alt={name} />
+      <p className="status">
+        <b>Status:</b> {status}
+      </p>
+      <button>Start a conversation</button>
+      <p className="info">
+        Search for someone to start chatting with, or go to Contacts to see who
+        is available
+      </p>
+    </EmptyStyles>
+  );
 };
 
 export default Empty;
