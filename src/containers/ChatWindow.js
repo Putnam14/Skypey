4a89533320898078be5e046196c4d9fb2e1ff28c
Putnam14 <putnambr76@gmail.com>
2019-03-16 17:41:18 -0600

    Add Header and Chats components to chat window

--- src/containers/ChatWindow.js
+++ src/containers/ChatWindow.js
@@ -1,10 +1,24 @@
 import React from "react";
 import styled from "styled-components";
+import Chats from "../components/Chats";
+import Header from "../components/Header";
+import store from "../store";
 
 const ChatStyles = styled.div``;
 
 const ChatWindow = ({ activeUserId }) => {
-  return <ChatStyles>Convo for user id: {activeUserId}</ChatStyles>;
+  const state = store.getState();
+  const activeUser = state.contacts[activeUserId];
+  const activeMessages = state.conversations[activeUserId];
+  const messages = Object.keys(activeMessages).map(
+    key => activeMessages[key].text
+  );
+  return (
+    <ChatStyles>
+      <Header user={activeUser} />
+      <Chats messages={messages} />
+    </ChatStyles>
+  );
 };
 
 export default ChatWindow;
