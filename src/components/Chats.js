d0df9886ebf1b5196b53a813a99187cd8be520de
Putnam14 <putnambr76@gmail.com>
2019-03-16 18:06:33 -0600

    Style the chat messages

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,15 +1,42 @@
 import React from "react";
 import styled from "styled-components";
 
-const ChatStyles = styled.div``;
+const ChatsStyles = styled.div`
+  display: grid;
+  width: 85%;
+  margin: 0 auto;
+  p {
+    color: #fff;
+    padding: 1rem;
+    margin: 1rem 0;
+    background: linear-gradient(90deg, #1986d8, #7b9cc2);
+    max-width: 90%;
+    justify-self: left;
+    border-radius: 0 10px 10px 0;
+  }
+  .is_user_msg {
+    background: #2b2c33;
+    justify-self: right;
+    border-radius: 10px 0 0 10px;
+  }
+  @media (max-width: 576px) {
+    max-width: 60%;
+  }
+`;
+
+const Chat = ({ message }) => {
+  const { text, is_user_msg } = message;
+  console.log(message);
+  return <p className={is_user_msg ? "is_user_msg" : ""}>{text}</p>;
+};
 
 const Chats = ({ messages }) => {
   return (
-    <ChatStyles>
-      {messages.map(text => (
-        <p>{text}</p>
+    <ChatsStyles>
+      {messages.map(message => (
+        <Chat message={message} key={message.number} />
       ))}
-    </ChatStyles>
+    </ChatsStyles>
   );
 };
 
