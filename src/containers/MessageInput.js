2eec1725d1b2db451410778003db1698ac99f8fb
Putnam14 <putnambr76@gmail.com>
2019-03-17 19:09:32 -0600

    Stub out MessageInput component

---
+++ src/containers/MessageInput.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const MessageInputStyles = styled.div``;
+
+const MessageInput = () => <p>Message input goes here</p>;
+
+export default MessageInput;
