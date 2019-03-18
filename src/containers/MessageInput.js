920d48b13b94240f19e93bc5a49e4414eed61327
Putnam14 <putnambr76@gmail.com>
2019-03-17 19:18:02 -0600

    Make the MessageInput pretty

--- src/containers/MessageInput.js
+++ src/containers/MessageInput.js
@@ -3,7 +3,20 @@ import styled from "styled-components";
 import store from "../store";
 import { setTypingValue } from "../actions";
 
-const MessageInputStyles = styled.form``;
+const MessageInputStyles = styled.form`
+  width: 80%;
+  margin: 1rem auto;
+  input {
+    width: 100%;
+    border: 0;
+    border-radius: 10px;
+    padding: 1rem;
+    background: rgba(0, 0, 0, 0.8);
+    color: #fff;
+    font-size: 1rem;
+    outline: 0;
+  }
+`;
 
 const MessageInput = ({ value }) => {
   const handleChange = e => {
